55c98daa987bf82ad922e0c0be30af7a89471c49
pierre-elie fortin <fortin.p@hotmail.fr>
2022-04-20 14:36:00 +0200

    delete unuse component

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -25,22 +25,6 @@ import SupportIcon from "./icons/IconSupport.vue";
     </template>
     <template #heading>Tooling</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite</a>.
-    The recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>.
-    If you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank">Cypress</a> and
-    <a
-      href="https://docs.cypress.io/guides/component-testing/introduction"
-      target="_blank"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
   </WelcomeItem>
 
   <WelcomeItem>
