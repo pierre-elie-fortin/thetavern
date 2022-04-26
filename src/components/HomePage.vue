a892f1eaeb23613c6deb84eac83e06210057bc6f
pierre-elie fortin <fortin.p@hotmail.fr>
2022-04-26 16:03:45 +0200

    add list events

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Logo from "@/components/Logo/Logo.vue";
 import Beers from "@/components/beers/Beers.vue";
+import Events from "@/components/events/Events.vue";
 </script>
 
 <template>
   <header>
     <div class="wrapper">
       <Logo></Logo>
+      <Events></Events>
       <Beers></Beers>
     </div>
   </header>
